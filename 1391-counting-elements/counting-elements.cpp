a6f0e2e8f1eef3867f8a50dc33df2f389b98828b
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-20 15:24:25 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (99.69%) - LeetSync

--- 1391-counting-elements/counting-elements.cpp
+++ 1391-counting-elements/counting-elements.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     int countElements(vector<int>& arr) {
-        unordered_set<int> hashSet(arr.begin(), arr.end());
+        std::sort(arr.begin(), arr.end());
         int count = 0;
-        for (int x : arr) {
-            if (hashSet.find(x + 1) != hashSet.end()) {
-                count++;
+        int runLength = 1;
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i - 1] != arr[i]) {
+                if (arr[i - 1] + 1 == arr[i]) {
+                    count += runLength;
+                }
+                runLength = 0;
             }
+            runLength++;
         }
         return count;
     }
