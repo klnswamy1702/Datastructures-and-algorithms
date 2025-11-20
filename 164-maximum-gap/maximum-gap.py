94061117c1db37edab2a491b0dab6401ccd31fc7
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-20 11:32:42 +0530

    Time: 1025 ms (5.01%) | Memory: 29.1 MB (65.58%) - LeetSync

--- 164-maximum-gap/maximum-gap.py
+++ 164-maximum-gap/maximum-gap.py
@@ -1,34 +1,29 @@
-class Bucket:
-    def __init__(self):
-        self.used = False
-        self.minval = float("inf")
-        self.maxval = float("-inf")
-
-
 class Solution:
     def maximumGap(self, nums):
         if len(nums) < 2:
             return 0
 
-        mini, maxi = min(nums), max(nums)
+        maxVal = max(nums)
+        exp = 1
+        radix = 10
+        aux = [0] * len(nums)
 
-        bucketSize = max(1, (maxi - mini) // (len(nums) - 1))
-        bucketNum = (maxi - mini) // bucketSize + 1
-        buckets = [Bucket() for _ in range(bucketNum)]
+        while maxVal // exp > 0:
+            count = [0] * radix
+            for num in nums:
+                count[(num // exp) % 10] += 1
+            for i in range(1, radix):
+                count[i] += count[i - 1]
+            i = len(nums) - 1
+            while i >= 0:
+                aux[count[(nums[i] // exp) % 10] - 1] = nums[i]
+                count[(nums[i] // exp) % 10] -= 1
+                i -= 1
+            for i in range(len(nums)):
+                nums[i] = aux[i]
+            exp *= 10
 
-        for num in nums:
-            idx = (num - mini) // bucketSize
-            buckets[idx].used = True
-            buckets[idx].minval = min(num, buckets[idx].minval)
-            buckets[idx].maxval = max(num, buckets[idx].maxval)
-
-        prevBucketMax = mini
         maxGap = 0
-        for bucket in buckets:
-            if not bucket.used:
-                continue
-
-            maxGap = max(maxGap, bucket.minval - prevBucketMax)
-            prevBucketMax = bucket.maxval
-
+        for i in range(len(nums) - 1):
+            maxGap = max(nums[i + 1] - nums[i], maxGap)
         return maxGap
