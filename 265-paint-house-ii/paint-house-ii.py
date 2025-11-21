7578da235334c2dc050377e57b3e3cb88d09c8b7
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-21 13:40:07 +0530

    Time: 3 ms (96.15%) | Memory: 18 MB (46.63%) - LeetSync

---
+++ 265-paint-house-ii/paint-house-ii.py
@@ -0,0 +1,41 @@
+class Solution:
+    def minCostII(self, costs: List[List[int]]) -> int:
+        n = len(costs)
+        if n == 0: return 0 # This is a valid case.
+        k = len(costs[0])
+
+        # Firstly, we need to determine the 2 lowest costs of
+        # the first row. We also need to remember the color of
+        # the lowest.
+        prev_min_cost = prev_second_min_cost = prev_min_color = None
+        for color, cost in enumerate(costs[0]):
+            if prev_min_cost is None or cost < prev_min_cost:
+                prev_second_min_cost = prev_min_cost
+                prev_min_color = color
+                prev_min_cost = cost
+            elif prev_second_min_cost is None or cost < prev_second_min_cost:
+                prev_second_min_cost = cost
+
+        # And now, we need to work our way down, keeping track of the minimums.
+        for house in range(1, n):
+            min_cost = second_min_cost = min_color = None
+            for color in range(k):
+                # Determime cost for this cell (without writing it into input array.)
+                cost = costs[house][color]
+                if color == prev_min_color:
+                    cost += prev_second_min_cost
+                else:
+                    cost += prev_min_cost
+                # And work out whether or not it is a current minimum.
+                if min_cost is None or cost < min_cost:
+                    second_min_cost = min_cost
+                    min_color = color
+                    min_cost = cost
+                elif second_min_cost is None or cost < second_min_cost:
+                    second_min_cost = cost
+            # Transfer currents to be prevs.
+            prev_min_cost = min_cost
+            prev_min_color = min_color
+            prev_second_min_cost = second_min_cost
+
+        return prev_min_cost
