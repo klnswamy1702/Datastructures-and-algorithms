88e9578f3853988d79a6f2fc602d2cf212bc00b4
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-21 13:42:00 +0530

    Time: 66 ms (6.67%) | Memory: 10.3 MB (6.67%) - LeetSync

--- 1213-handshakes-that-dont-cross/handshakes-that-dont-cross.cpp
+++ 1213-handshakes-that-dont-cross/handshakes-that-dont-cross.cpp
@@ -3,13 +3,20 @@ class Solution {
 
 public:
     int numberOfWays(int numPeople) {
-        vector<int> dp(numPeople / 2 + 1);
+        vector<int> dp(numPeople / 2 + 1, -1);
         dp[0] = 1;
-        for (int i = 1; i <= numPeople / 2; i++) {
-            for (int j = 0; j < i; j++) {
-                (dp[i] += (long long)dp[j] * dp[i - j - 1] % m) %= m;
+        
+        function<int(int)> calculateDP = [&](int i) -> int {
+            if (dp[i] != -1) {
+                return dp[i];
             }
-        }
-        return dp[numPeople / 2];
+            dp[i] = 0;
+            for (int j = 0; j < i; j++) {
+                (dp[i] += (long long)calculateDP(j) * calculateDP(i - j - 1) % m) %= m;
+            }
+            return dp[i];
+        };
+        
+        return calculateDP(numPeople / 2);
     }
 };
