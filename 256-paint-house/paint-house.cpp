a8c0ade061300af63e0637c2290533b51d471fcd
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-21 13:39:23 +0530

    Time: 3 ms (22.83%) | Memory: 13.3 MB (73.20%) - LeetSync

--- 256-paint-house/paint-house.cpp
+++ 256-paint-house/paint-house.cpp
@@ -3,14 +3,19 @@ public:
     int minCost(vector<vector<int>>& costs) {
         if (costs.size() == 0) return 0;
 
-        vector<vector<int>> memo = {costs.size() + 1, vector<int>(3, 0)};
+        vector<int> previousRow = costs[costs.size() - 1];
 
-        for (int i = costs.size() - 1; i >= 0; --i) {
-            memo[i][0] = costs[i][0] + min(memo[i + 1][1], memo[i + 1][2]);
-            memo[i][1] = costs[i][1] + min(memo[i + 1][0], memo[i + 1][2]);
-            memo[i][2] = costs[i][2] + min(memo[i + 1][0], memo[i + 1][1]);
+        for (int n = costs.size() - 2; n >= 0; n--) {
+            vector<int> currentRow = costs[n];
+            // Total cost of painting the nth house red.
+            currentRow[0] += min(previousRow[1], previousRow[2]);
+            // Total cost of painting the nth house green.
+            currentRow[1] += min(previousRow[0], previousRow[2]);
+            // Total cost of painting the nth house blue.
+            currentRow[2] += min(previousRow[0], previousRow[1]);
+            previousRow = currentRow;
         }
 
-        return min(min(memo[0][0], memo[0][1]), memo[0][2]);
+        return min({previousRow[0], previousRow[1], previousRow[2]});
     }
 };
