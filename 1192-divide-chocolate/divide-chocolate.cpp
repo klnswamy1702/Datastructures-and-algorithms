f3bdcf6c3788432d7eb159b65be5cb30b58a46f4
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-21 13:19:44 +0530

    Time: 3 ms (61.46%) | Memory: 19.1 MB (38.54%) - LeetSync

--- 1192-divide-chocolate/divide-chocolate.cpp
+++ 1192-divide-chocolate/divide-chocolate.cpp
@@ -1,21 +1,44 @@
 class Solution {
 public:
-        int maximizeSweetness(vector<int>& A, int K) {
-        int left = 1, right = 1e9 / (K + 1);
-        while (left < right) {
+    int maximizeSweetness(vector<int>& sweetness, int k) {
+        // Initialize the left and right boundaries.
+        // left = 1 and right = total sweetness / number of people.
+        int numberOfPeople = k + 1;
+        int left = *min_element(sweetness.begin(), sweetness.end());
+        int right = accumulate(sweetness.begin(), sweetness.end(), 0) / numberOfPeople;
+        
+        while (left < right) { 
+            // Get the middle index between left and right boundary indexes.
+            // cur_sweetness stands for the total sweetness for the current person.
+            // people_with_chocolate stands for the number of people that have 
+            // a piece of chocolate of sweetness greater than or equal to mid.  
             int mid = (left + right + 1) / 2;
-            int cur = 0, cuts = 0;
-            for (int a : A) {
-                if ((cur += a) >= mid) {
-                    cur = 0;
-                    if (++cuts > K) break;
+            int curSweetness = 0;
+            int peopleWithChocolate = 0;
+            
+            // Start assigning chunks to the current people,.
+            for (int s : sweetness) {
+                curSweetness += s;
+                
+                // When the total sweetness from him is no less than mid, meaning we 
+                // have done with him and move on to assigning chunks to the next people.
+                if (curSweetness >= mid) {
+                    peopleWithChocolate += 1;
+                    curSweetness = 0;
                 }
             }
-            if (cuts > K)
+            
+            // Check if we successfully give everyone a piece of chocolate with sweetness
+            // no less than mid, and eliminate the search space by half.
+            if (peopleWithChocolate >= numberOfPeople) {
                 left = mid;
-            else
+            } else {
                 right = mid - 1;
+            }
         }
-        return left;
+        
+        // Once the left and right boundaries coincide, we find the target value,
+        // that is, the maximum possible sweetness we can get.
+        return right;
     }
 };
