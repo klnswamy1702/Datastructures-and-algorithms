9bf8de022035a756cb4a9f7fa25f0df1f5487c41
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-21 13:39:29 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (85.11%) - LeetSync

--- 256-paint-house/paint-house.cpp
+++ 256-paint-house/paint-house.cpp
@@ -1,3 +1,5 @@
+// This code OVERWRITES the input vector!
+
 class Solution {
 public:
     int minCost(vector<vector<int>>& costs) {
@@ -6,16 +8,21 @@ public:
         vector<int> previousRow = costs[costs.size() - 1];
 
         for (int n = costs.size() - 2; n >= 0; n--) {
-            vector<int> currentRow = costs[n];
-            // Total cost of painting the nth house red.
+            // PROBLEMATIC CODE IS HERE
+            // This line here is NOT making a copy of the original, it's simply
+            // making a reference to it Therefore, any writes into currentRow
+            // will also be written into "costs". This is not what we wanted!
+            vector<int>& currentRow = costs[n];
+
+            // Total cost of painting nth house red?
             currentRow[0] += min(previousRow[1], previousRow[2]);
-            // Total cost of painting the nth house green.
+            // Total cost of painting nth house green?
             currentRow[1] += min(previousRow[0], previousRow[2]);
-            // Total cost of painting the nth house blue.
+            // Total cost of painting nth house blue?
             currentRow[2] += min(previousRow[0], previousRow[1]);
             previousRow = currentRow;
         }
 
-        return min({previousRow[0], previousRow[1], previousRow[2]});
+        return min(min(previousRow[0], previousRow[1]), previousRow[2]);
     }
 };
