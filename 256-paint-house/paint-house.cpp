c29bf4ff1854bc6a1303bc80a376072796e35ab6
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-21 13:37:27 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (9.68%) - LeetSync

--- 256-paint-house/paint-house.cpp
+++ 256-paint-house/paint-house.cpp
@@ -1,38 +1,16 @@
 class Solution {
-private:
-    vector<vector<int>> costs;
-    unordered_map<string, int> memo;
-
 public:
-    int minCost(vector<vector<int>>& _costs) {
-        if (_costs.size() == 0) {
-            return 0;
-        }
-        this->costs = _costs;
-        return min(paintCost(0, 0), min(paintCost(0, 1), paintCost(0, 2)));
-    }
+    int minCost(vector<vector<int>>& costs) {
+        if (costs.size() == 0) return 0;
 
-private:
-    int paintCost(int n, int color) {
-        string key = getKey(n, color);
-        if (memo.find(key) != memo.end()) {
-            return memo[key];
-        }
-        int totalCost = costs[n][color];
-        if (n == costs.size() - 1) {
-        } else if (color == 0) {  // Red
-            totalCost += min(paintCost(n + 1, 1), paintCost(n + 1, 2));
-        } else if (color == 1) {  // Green
-            totalCost += min(paintCost(n + 1, 0), paintCost(n + 1, 2));
-        } else {  // Blue
-            totalCost += min(paintCost(n + 1, 0), paintCost(n + 1, 1));
-        }
-        memo[key] = totalCost;
+        vector<vector<int>> memo = {costs.size() + 1, vector<int>(3, 0)};
 
-        return totalCost;
-    }
+        for (int i = costs.size() - 1; i >= 0; --i) {
+            memo[i][0] = costs[i][0] + min(memo[i + 1][1], memo[i + 1][2]);
+            memo[i][1] = costs[i][1] + min(memo[i + 1][0], memo[i + 1][2]);
+            memo[i][2] = costs[i][2] + min(memo[i + 1][0], memo[i + 1][1]);
+        }
 
-    string getKey(int n, int color) {
-        return to_string(n) + " " + to_string(color);
+        return min(min(memo[0][0], memo[0][1]), memo[0][2]);
     }
 };
