2bf1434f184521d06670fda2d4837b07d4e26c59
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-21 13:11:25 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (41.58%) - LeetSync

--- 311-sparse-matrix-multiplication/sparse-matrix-multiplication.cpp
+++ 311-sparse-matrix-multiplication/sparse-matrix-multiplication.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
+    vector<vector<pair<int, int>>> compressMatrix(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<vector<pair<int, int>>> compressedMatrix(rows);
+        
+        for (int row = 0; row < rows; ++row) {
+            for (int col = 0; col < cols; ++col) {
+                if (matrix[row][col] != 0) {
+                    compressedMatrix[row].push_back({matrix[row][col], col}); 
+                }
+            }
+        }
+        return compressedMatrix;
+    }
+    
     vector<vector<int>> multiply(vector<vector<int>>& mat1, vector<vector<int>>& mat2) {
-        int n = mat1.size();
+        int m = mat1.size();
         int k = mat1[0].size();
-        int m = mat2[0].size();
+        int n = mat2[0].size();
         
-        // Product matrix will have 'n x m' size.
-        vector<vector<int>> ans (n, vector<int>(m, 0));
+        // Store the non-zero values of each matrix.
+        vector<vector<pair<int, int>>> A = compressMatrix(mat1);
+        vector<vector<pair<int, int>>> B = compressMatrix(mat2);
         
-        for (int rowIndex = 0; rowIndex < n; ++rowIndex) {
-            for (int elementIndex = 0; elementIndex < k; ++elementIndex) {
-                // If current element of mat1 is non-zero then iterate over all columns of mat2.
-                if (mat1[rowIndex][elementIndex] != 0)  {
-                    for (int colIndex = 0; colIndex < m; ++colIndex) {
-                        ans[rowIndex][colIndex] += mat1[rowIndex][elementIndex] * mat2[elementIndex][colIndex];
-                    }
+        vector<vector<int>> ans(m, vector<int>(n, 0));
+        
+        for (int mat1Row = 0; mat1Row < m; ++mat1Row) {
+            // Iterate on all current 'row' non-zero elements of mat1.
+            for (auto [element1, mat1Col] : A[mat1Row]) {
+
+                // Multiply and add all non-zero elements of mat2
+                // where the row is equal to col of current element of mat1.
+                for (auto [element2, mat2Col] : B[mat1Col]) {
+                    ans[mat1Row][mat2Col] += element1 * element2;
                 }
             }
         }
