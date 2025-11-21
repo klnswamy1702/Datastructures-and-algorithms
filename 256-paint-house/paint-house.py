f94c728cbead477973f7cc32c1c05a269a506ee7
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-21 13:37:49 +0530

    Time: 4 ms (47.11%) | Memory: 18.3 MB (12.01%) - LeetSync

--- 256-paint-house/paint-house.py
+++ 256-paint-house/paint-house.py
@@ -1,26 +1,25 @@
-from typing import List
+from functools import lru_cache
 
 class Solution:
-    def minCost(self, costs: List[List[int]]) -> int:
-        self.memo = {}
+    def minCost(self, costs):
+        """
+        :type costs: List[List[int]]
+        :rtype: int
+        """
 
-        def paint_cost(n: int, color: int) -> int:
-            if (n, color) in self.memo:
-                return self.memo[(n, color)]
-            
+        @lru_cache(maxsize=None)
+        def paint_cost(n, color):
             total_cost = costs[n][color]
-            if n < len(costs) - 1:
-                if color == 0:
-                    total_cost += min(paint_cost(n + 1, 1), paint_cost(n + 1, 2))
-                elif color == 1:
-                    total_cost += min(paint_cost(n + 1, 0), paint_cost(n + 1, 2))
-                else:
-                    total_cost += min(paint_cost(n + 1, 0), paint_cost(n + 1, 1))
-
-            self.memo[(n, color)] = total_cost
+            if n == len(costs) - 1:
+                pass
+            elif color == 0:
+                total_cost += min(paint_cost(n + 1, 1), paint_cost(n + 1, 2))
+            elif color == 1:
+                total_cost += min(paint_cost(n + 1, 0), paint_cost(n + 1, 2))
+            else:
+                total_cost += min(paint_cost(n + 1, 0), paint_cost(n + 1, 1))
             return total_cost
 
-        if not costs:
+        if costs == []:
             return 0
-
         return min(paint_cost(0, 0), paint_cost(0, 1), paint_cost(0, 2))
