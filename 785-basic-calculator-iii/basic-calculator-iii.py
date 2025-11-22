84c6019658adb10b9bd6905e1a1c1b646e15944f
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-22 19:00:56 +0530

    Time: 3 ms (94.23%) | Memory: 18 MB (28.43%) - LeetSync

--- 785-basic-calculator-iii/basic-calculator-iii.py
+++ 785-basic-calculator-iii/basic-calculator-iii.py
@@ -9,33 +9,28 @@ class Solution:
                 return x * y
             return int(x / y)
         
-        def solve(i):
-            stack = []
-            curr = 0
-            previous_operator = "+"
-            
-            while i[0] < len(s):
-                c = s[i[0]]
-                if c == "(":
-                    i[0] += 1
-                    curr = solve(i)
-                elif c.isdigit():
-                    curr = curr * 10 + int(c)
-                else:
-                    if previous_operator in "*/":
-                        stack.append(evaluate(stack.pop(), curr, previous_operator))
-                    else:
-                        stack.append(evaluate(curr, 0, previous_operator))
-                     
-                    if c == ")":
-                        break
-                    
-                    curr = 0
-                    previous_operator = c
-                    
-                i[0] += 1
-            
-            return sum(stack)
-
+        stack = []
+        curr = 0
+        previous_operator = "+"
         s += "@"
-        return solve([0])
+        
+        for c in s:
+            if c.isdigit():
+                curr = curr * 10 + int(c)
+            elif c == "(":
+                stack.append(previous_operator)
+                previous_operator = "+"
+            else:
+                if previous_operator in "*/":
+                    stack.append(evaluate(stack.pop(), curr, previous_operator))
+                else:
+                    stack.append(evaluate(curr, 0, previous_operator))
+                
+                curr = 0
+                previous_operator = c
+                if c == ")":
+                    while type(stack[-1]) == int:
+                        curr += stack.pop()
+                    previous_operator = stack.pop()
+
+        return sum(stack)
