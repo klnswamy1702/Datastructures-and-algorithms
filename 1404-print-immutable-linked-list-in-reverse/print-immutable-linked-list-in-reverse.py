cd9bf3c0c4ce27f0494a00609516db2fbd10033d
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-22 18:52:10 +0530

    Time: 48 ms (15.33%) | Memory: 18 MB (37.33%) - LeetSync

--- 1404-print-immutable-linked-list-in-reverse/print-immutable-linked-list-in-reverse.py
+++ 1404-print-immutable-linked-list-in-reverse/print-immutable-linked-list-in-reverse.py
@@ -8,7 +8,11 @@
 
 class Solution:
     def printLinkedListInReverse(self, head: 'ImmutableListNode') -> None:
-        if head is not None:
-            self.printLinkedListInReverse(head.getNext())
-            head.printValue()
-        
+        stack = []
+        while head:
+            stack.append(head)
+            head = head.getNext()
+
+        while stack:
+            node = stack.pop()
+            node.printValue()
