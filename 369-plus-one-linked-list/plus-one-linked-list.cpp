f78f6a07b888821be8f2698b552d774dc6bfcb77
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-22 18:55:53 +0530

    Time: 5 ms (30.11%) | Memory: 12.2 MB (35.48%) - LeetSync

---
+++ 369-plus-one-linked-list/plus-one-linked-list.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* plusOne(ListNode* head) {
+        // sentinel head
+        ListNode* sentinel = new ListNode(0);
+        sentinel->next = head;
+        ListNode* notNine = sentinel;
+
+        // find the rightmost not-nine digit
+        while (head != nullptr) {
+            if (head->val != 9) notNine = head;
+            head = head->next;
+        }
+        // increase this rightmost not-nine digit by 1
+        notNine->val++;
+        notNine = notNine->next;
+        // set all the following nines to zeros
+        while (notNine != nullptr) {
+            notNine->val = 0;
+            notNine = notNine->next;
+        }
+
+        delete notNine;
+        return sentinel->val != 0 ? sentinel : sentinel->next;
+    }
+};
