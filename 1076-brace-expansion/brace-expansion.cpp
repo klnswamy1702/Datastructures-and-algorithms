e83d0b98ad08fa745e9777bb29be18f28b39591e
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-24 00:28:29 +0530

    Time: 4 ms (34.56%) | Memory: 11.8 MB (80.15%) - LeetSync

--- 1076-brace-expansion/brace-expansion.cpp
+++ 1076-brace-expansion/brace-expansion.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int storeFirstOptions(string& s, int startPos, vector<char>& firstOptions) {
+    int storeFirstOptions(string& s, int startPos, vector<string>& firstOptions) {
         // If the first character is not '{', it means a single character
         if (s[startPos] != '{') {
-            firstOptions.push_back(s[startPos]);
+            firstOptions.push_back(string(1, s[startPos]));
         } else {
             // Store all the characters between '{' and '}'
             while (s[startPos] != '}') {
                  if (s[startPos] >= 'a' && s[startPos] <= 'z') {
-                     firstOptions.push_back(s[startPos]);
+                     firstOptions.push_back(string(1, s[startPos]));
                  }
                 startPos++;
             }
@@ -19,28 +19,27 @@ public:
         return startPos + 1;
     }
     
-    vector<string> findAllWords(string& s, int startPos) {
-        // Return empty string list if the string is empty
-        if (startPos == s.size()) {
-            return {""};
-        }
+    vector<string> expand(string s) {
+        vector<string> expandedWords = {""};
         
-        vector<char> firstOptions;
-        // Store the characters for the first index as string in firstOptions
-        int remStringStartPos = storeFirstOptions(s, startPos, firstOptions);
-        vector<string> wordsWithRemString = findAllWords(s, remStringStartPos);
-        
-        vector<string> expandedWords;
-        // Create new words by adding the character at the beginning
-        for (char c : firstOptions) {
-            for (string word : wordsWithRemString) {
-                expandedWords.push_back(c + word);
+        int startPos = 0;
+        while (startPos < s.size()) {
+            vector<string> firstOptions;
+            // Store the characters for the first index as string in firstOptions
+            int remStringStartPos = storeFirstOptions(s, startPos, firstOptions);
+            
+            vector<string> currWords;
+            // Append the string in the list firstOptions to string in expandedWords
+            for (string word : expandedWords) {
+                for (string c : firstOptions) {
+                    currWords.push_back(word + c);
+                }
             }
+            // Update the list expandedWords to have all the words
+            expandedWords = currWords;
+            // Pointing to the next character to be considered
+            startPos = remStringStartPos;
         }
         return expandedWords;
-    }
-    
-    vector<string> expand(string s) {
-        return findAllWords(s, 0);
     }
 };
