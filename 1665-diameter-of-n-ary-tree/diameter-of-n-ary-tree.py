65fde3f61c091153910387792f4d61be57dee975
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-24 00:38:52 +0530

    Time: 41 ms (89.07%) | Memory: 19.1 MB (41.09%) - LeetSync

--- 1665-diameter-of-n-ary-tree/diameter-of-n-ary-tree.py
+++ 1665-diameter-of-n-ary-tree/diameter-of-n-ary-tree.py
@@ -7,29 +7,35 @@ class Node:
 """
 class Solution:
     def diameter(self, root: 'Node') -> int:
+        """
+        :type root: 'Node'
+        :rtype: int
+        """
         diameter = 0
 
-        def height(node):
-            """ return the height of the node """
+        def maxDepth(node, curr_depth):
+            """ return the maximum depth of leaves nodes
+                 descending from the current node
+            """
             nonlocal diameter
 
             if len(node.children) == 0:
-                return 0
-
-            # select the top two heights
-            max_height_1, max_height_2 = 0, 0
+                return curr_depth
+            
+            # select the top 2 depths from its children
+            max_depth_1, max_depth_2 = curr_depth, 0
             for child in node.children:
-                parent_height = height(child) + 1
-                if parent_height > max_height_1:
-                    max_height_1, max_height_2 = parent_height, max_height_1
-                elif parent_height > max_height_2:
-                    max_height_2 = parent_height
+                depth = maxDepth(child, curr_depth+1)
+                if depth > max_depth_1:
+                    max_depth_1, max_depth_2 = depth, max_depth_1
+                elif depth > max_depth_2:
+                    max_depth_2 = depth
 
-            # calculate the distance between the two farthest leaves nodes.
-            distance = max_height_1 + max_height_2
+            # calculate the distance between the two farthest leaves nodes
+            distance = max_depth_1 + max_depth_2 - 2 * curr_depth
             diameter = max(diameter, distance)
 
-            return max_height_1
+            return max_depth_1
 
-        height(root)
+        maxDepth(root, 0)
         return diameter
