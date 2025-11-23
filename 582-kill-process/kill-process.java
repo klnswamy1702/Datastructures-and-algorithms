7d09dd69160735d3a094e03fd2aace0d13214419
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-24 00:53:42 +0530

    Time: 30 ms (45.38%) | Memory: 62.8 MB (9.23%) - LeetSync

--- 582-kill-process/kill-process.java
+++ 582-kill-process/kill-process.java
@@ -1,24 +1,30 @@
 public class Solution {
-
+    class Node {
+        int val;
+        List < Node > children = new ArrayList < > ();
+    }
     public List < Integer > killProcess(List < Integer > pid, List < Integer > ppid, int kill) {
-        HashMap < Integer, List < Integer >> map = new HashMap < > ();
+        HashMap < Integer, Node > map = new HashMap < > ();
+        for (int id: pid) {
+            Node node = new Node();
+            node.val = id;
+            map.put(id, node);
+        }
         for (int i = 0; i < ppid.size(); i++) {
             if (ppid.get(i) > 0) {
-                List < Integer > l = map.getOrDefault(ppid.get(i), new ArrayList < Integer > ());
-                l.add(pid.get(i));
-                map.put(ppid.get(i), l);
+                Node par = map.get(ppid.get(i));
+                par.children.add(map.get(pid.get(i)));
             }
         }
-        Queue < Integer > queue = new LinkedList < > ();
         List < Integer > l = new ArrayList < > ();
-        queue.add(kill);
-        while (!queue.isEmpty()) {
-            int r = queue.remove();
-            l.add(r);
-            if (map.containsKey(r))
-                for (int id: map.get(r))
-                    queue.add(id);
-        }
+        l.add(kill);
+        getAllChildren(map.get(kill), l);
         return l;
+    }
+    public void getAllChildren(Node pn, List < Integer > l) {
+        for (Node n: pn.children) {
+            l.add(n.val);
+            getAllChildren(n, l);
+        }
     }
 }
