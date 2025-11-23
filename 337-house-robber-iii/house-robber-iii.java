ab7d1cdcbbd8e0d343f3cc02e94391412826aca9
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-23 19:05:33 +0530

    Time: 5 ms (5.54%) | Memory: 47.8 MB (5.26%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.java
+++ 337-house-robber-iii/house-robber-iii.java
@@ -1,31 +1,56 @@
 class Solution {
-    HashMap<TreeNode, Integer> robResult = new HashMap<>();
-    HashMap<TreeNode, Integer> notRobResult = new HashMap<>();
-
-    public int helper(TreeNode node, boolean parentRobbed) {
-        if (node == null) {
+    public int rob(TreeNode root) {
+        if (root == null) {
             return 0;
         }
-        if (parentRobbed) {
-            if (robResult.containsKey(node)) {
-                return robResult.get(node);
-            }
-            int result = helper(node.left, false) + helper(node.right, false);
-            robResult.put(node, result);
-            return result;
-        } else {
-            if (notRobResult.containsKey(node)) {
-                return notRobResult.get(node);
-            }
-            int rob = node.val + helper(node.left, true) + helper(node.right, true);
-            int notRob = helper(node.left, false) + helper(node.right, false);
-            int result = Math.max(rob, notRob);
-            notRobResult.put(node, result);
-            return result;
-        }
-    }
+        // reform tree into array-based tree
+        ArrayList<Integer> tree = new ArrayList<>();
+        HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
+        graph.put(-1, new ArrayList<>());
+        int index = -1;
+        // we use two Queue to store node and index
+        Queue<TreeNode> q_node = new LinkedList<>();
+        q_node.add(root);
+        Queue<Integer> q_index = new LinkedList<>();
+        q_index.add(index);
 
-    public int rob(TreeNode root) {
-        return helper(root, false);
+        while (q_node.size() > 0) {
+            TreeNode node = q_node.poll();
+            int parentIndex = q_index.poll();
+            if (node != null) {
+                index++;
+                tree.add(node.val);
+                graph.put(index, new ArrayList<>());
+                graph.get(parentIndex).add(index);
+                // push new node into Queue
+                q_node.add(node.left);
+                q_index.add(index);
+                q_node.add(node.right);
+                q_index.add(index);
+            }
+        }
+
+        // represent the maximum start by node i with robbing i
+        int[] dpRob = new int[index + 1];
+
+        // represent the maximum start by node i without robbing i
+        int[] dpNotRob = new int[index + 1];
+
+        for (int i = index; i >= 0; i--) {
+            ArrayList<Integer> children = graph.get(i);
+            if (children == null || children.size() == 0) {
+                // if is leaf
+                dpRob[i] = tree.get(i);
+                dpNotRob[i] = 0;
+            } else {
+                dpRob[i] = tree.get(i);
+                for (int child : children) {
+                    dpRob[i] += dpNotRob[child];
+                    dpNotRob[i] += Math.max(dpRob[child], dpNotRob[child]);
+                }
+            }
+        }
+
+        return Math.max(dpRob[0], dpNotRob[0]);
     }
 }
