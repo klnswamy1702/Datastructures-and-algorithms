31c63814d5093884efc1f119e55bd7b01795b09d
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-23 18:40:41 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (22.33%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -16,6 +16,12 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 from collections import defaultdict
 class Solution:
     def verticalOrder(self, root: TreeNode) -> List[List[int]]:
@@ -24,17 +30,25 @@ class Solution:
 
         columnTable = defaultdict(list)
         min_column = max_column = 0
-        queue = deque([(root, 0)])
 
-        while queue:
-            node, column = queue.popleft()
-
+        def DFS(node, row, column):
             if node is not None:
-                columnTable[column].append(node.val)
+                nonlocal min_column, max_column
+                columnTable[column].append((row, node.val))
                 min_column = min(min_column, column)
                 max_column = max(max_column, column)
 
-                queue.append((node.left, column - 1))
-                queue.append((node.right, column + 1))
+                # preorder DFS
+                DFS(node.left, row + 1, column - 1)
+                DFS(node.right, row + 1, column + 1)
 
-        return [columnTable[x] for x in range(min_column, max_column + 1)]
+        DFS(root, 0, 0)
+
+        # order by column and sort by row
+        ret = []
+        for col in range(min_column, max_column + 1):
+            columnTable[col].sort(key=lambda x:x[0])
+            colVals = [val for row, val in columnTable[col]]
+            ret.append(colVals)
+
+        return ret
