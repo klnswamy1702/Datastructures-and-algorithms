f398bcc0e273562d5a2b4bf28ef7c8b468112813
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-24 00:53:06 +0530

    Time: 31 ms (31.54%) | Memory: 53.6 MB (65.38%) - LeetSync

--- 582-kill-process/kill-process.java
+++ 582-kill-process/kill-process.java
@@ -1,30 +1,23 @@
 public class Solution {
-    class Node {
-        int val;
-        List < Node > children = new ArrayList < > ();
-    }
     public List < Integer > killProcess(List < Integer > pid, List < Integer > ppid, int kill) {
-        HashMap < Integer, Node > map = new HashMap < > ();
-        for (int id: pid) {
-            Node node = new Node();
-            node.val = id;
-            map.put(id, node);
-        }
+        HashMap < Integer, List < Integer >> map = new HashMap < > ();
         for (int i = 0; i < ppid.size(); i++) {
             if (ppid.get(i) > 0) {
-                Node par = map.get(ppid.get(i));
-                par.children.add(map.get(pid.get(i)));
+                List < Integer > l = map.getOrDefault(ppid.get(i), new ArrayList < Integer > ());
+                l.add(pid.get(i));
+                map.put(ppid.get(i), l);
             }
         }
         List < Integer > l = new ArrayList < > ();
         l.add(kill);
-        getAllChildren(map.get(kill), l);
+        getAllChildren(map, l, kill);
         return l;
     }
-    public void getAllChildren(Node pn, List < Integer > l) {
-        for (Node n: pn.children) {
-            l.add(n.val);
-            getAllChildren(n, l);
-        }
+    public void getAllChildren(HashMap < Integer, List < Integer >> map, List < Integer > l, int kill) {
+        if (map.containsKey(kill))
+            for (int id: map.get(kill)) {
+                l.add(id);
+                getAllChildren(map, l, id);
+            }
     }
 }
