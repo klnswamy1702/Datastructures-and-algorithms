4465e500306fee31f4c036660e97e7b9476075f2
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-23 21:57:07 +0530

    Time: 2 ms (62.37%) | Memory: 19.7 MB (73.61%) - LeetSync

--- 272-closest-binary-search-tree-value-ii/closest-binary-search-tree-value-ii.py
+++ 272-closest-binary-search-tree-value-ii/closest-binary-search-tree-value-ii.py
@@ -6,20 +6,27 @@
 #         self.right = right
 class Solution:
     def closestKValues(self, root: TreeNode, target: float, k: int) -> List[int]:
-        def dfs(node, heap):
+        def dfs(node, arr):
             if not node:
                 return
-
-            if len(heap) < k:
-                heappush(heap, (-abs(node.val - target), node.val))
+            
+            dfs(node.left, arr)
+            arr.append(node.val)
+            dfs(node.right, arr)
+        
+        arr = []
+        dfs(root, arr)
+        
+        left = bisect_left(arr, target) - 1
+        right = left + 1
+        ans = []
+        
+        while len(ans) < k:
+            if right == len(arr) or abs(arr[left] - target) <= abs(arr[right] - target):
+                ans.append(arr[left])
+                left -= 1
             else:
-                if abs(node.val - target) <= abs(heap[0][0]):
-                    heappop(heap)
-                    heappush(heap, (-abs(node.val - target), node.val))
-
-            dfs(node.left, heap)
-            dfs(node.right, heap)
-
-        heap = []
-        dfs(root, heap)
-        return [x[1] for x in heap]
+                ans.append(arr[right])
+                right += 1
+        
+        return ans
