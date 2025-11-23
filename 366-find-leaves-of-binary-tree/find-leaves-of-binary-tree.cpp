0ea6ef431c24e4e0cee744e10aacc84ac5f29708
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-23 18:41:34 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (61.68%) - LeetSync

--- 366-find-leaves-of-binary-tree/find-leaves-of-binary-tree.cpp
+++ 366-find-leaves-of-binary-tree/find-leaves-of-binary-tree.cpp
@@ -10,15 +10,19 @@
  * };
  */
 class Solution {
+private:
+
+    vector<vector<int>> solution;
+
 public:
-    
-    vector<pair<int, int>> pairs;
     
     int getHeight(TreeNode *root) {
         
         // return -1 for null nodes
-        if (!root) return -1;
-        
+        if (!root) {
+            return -1;
+        }
+
         // first calculate the height of the left and right children
         int leftHeight = getHeight(root->left);
         int rightHeight = getHeight(root->right);
@@ -26,32 +30,23 @@ public:
         // based on the height of the left and right children, obtain the height of the current (parent) node
         int currHeight = max(leftHeight, rightHeight) + 1;
         
-        // collect the pair -> (height, val)
-        this->pairs.push_back({currHeight, root->val});
+        // create space for node located at `currHeight` if not already exists
+        if (this->solution.size() == currHeight) {
+            this->solution.push_back({});
+        }
+
+        // insert the value at the correct position in the solution array
+        this->solution[currHeight].push_back(root->val);
         
         // return the height of the current node
         return currHeight;
     }
     
     vector<vector<int>> findLeaves(TreeNode* root) {
-        this->pairs.clear();
+        this->solution.clear();
         
         getHeight(root);
         
-        // sort all the (height, val) pairs
-        sort(this->pairs.begin(), this->pairs.end());
-        
-        int n = this->pairs.size(), height = 0, i = 0;
-        vector<vector<int>> solution;
-        while (i < n) {
-            vector<int> nums;
-            while (i < n && this->pairs[i].first == height) {
-                nums.push_back(this->pairs[i].second);
-                i++;
-            }
-            solution.push_back(nums);
-            height++;
-        }
-        return solution;
+        return this->solution;
     }
 };
