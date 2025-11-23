c5ff035e4e5b6050b69f1fdd514b2e5e8edcea24
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-23 18:32:33 +0530

    Time: 5 ms (58.93%) | Memory: 47.5 MB (12.50%) - LeetSync

--- 484-find-permutation/find-permutation.java
+++ 484-find-permutation/find-permutation.java
@@ -1,20 +1,22 @@
-
 public class Solution {
     public int[] findPermutation(String s) {
         int[] res = new int[s.length() + 1];
-        Stack < Integer > stack = new Stack < > ();
-        int j = 0;
-        for (int i = 1; i <= s.length(); i++) {
-            if (s.charAt(i - 1) == 'I') {
-                stack.push(i);
-                while (!stack.isEmpty())
-                    res[j++] = stack.pop();
-            } else
-                stack.push(i);
+        res[0]=1;
+        int i = 1;
+        while (i <= s.length()) {
+            res[i]=i+1;
+            int j = i;
+            if(s.charAt(i-1)=='D')
+            {
+                while (i <= s.length() && s.charAt(i - 1) == 'D')
+                    i++;
+                for (int k = j - 1, c = i; k <= i - 1; k++, c--) {
+                    res[k] = c;
+                }
+            }
+            else
+                i++;
         }
-        stack.push(s.length() + 1);
-        while (!stack.isEmpty())
-            res[j++] = stack.pop();
         return res;
     }
 }
