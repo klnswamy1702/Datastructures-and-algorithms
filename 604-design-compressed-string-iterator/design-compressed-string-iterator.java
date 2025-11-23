05b1e6dc4cb4067c01af5a2772136d86196580f0
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-24 00:23:35 +0530

    Time: 22 ms (6.54%) | Memory: 46.8 MB (8.41%) - LeetSync

---
+++ 604-design-compressed-string-iterator/design-compressed-string-iterator.java
@@ -0,0 +1,28 @@
+
+import java.util.regex.Pattern;
+public class StringIterator {
+    int ptr = 0;
+    String[] chars;int[] nums;
+    public StringIterator(String compressedString) {
+        nums = Arrays.stream(compressedString.substring(1).split("[a-zA-Z]+")).mapToInt(Integer::parseInt).toArray();;
+        chars = compressedString.split("[0-9]+");
+    }
+    public char next() {
+        if (!hasNext())
+            return ' ';
+        nums[ptr]--;
+        char res=chars[ptr].charAt(0);
+        if(nums[ptr]==0)
+            ptr++;
+        return res;
+    }
+    public boolean hasNext() {
+        return ptr != chars.length;
+    }
+}
+/**
+ * Your StringIterator object will be instantiated and called as such:
+ * StringIterator obj = new StringIterator(compressedString);
+ * char param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
