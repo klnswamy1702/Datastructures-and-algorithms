94ae20c422863c859f343d1a7290428946c2d251
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-24 00:19:38 +0530

    Time: 51 ms (38.91%) | Memory: 18.4 MB (6.56%) - LeetSync

--- 281-zigzag-iterator/zigzag-iterator.py
+++ 281-zigzag-iterator/zigzag-iterator.py
@@ -1,39 +1,26 @@
 class ZigzagIterator:
     def __init__(self, v1: List[int], v2: List[int]):
         self.vectors = [v1, v2]
-        self.p_elem = 0   # pointer to the index of element
-        self.p_vec = 0    # pointer to the vector
-        # variables for hasNext() function
-        self.total_num = len(v1) + len(v2)
-        self.output_count = 0
+        self.queue = deque()
+        for index, vector in enumerate(self.vectors):
+            # <index_of_vector, index_of_element_to_output>
+            if len(vector) > 0:
+                self.queue.append((index, 0))
 
     def next(self) -> int:
-        iter_num = 0
-        ret = None
 
-        # Iterate over the vectors
-        while iter_num < len(self.vectors):
-            curr_vec = self.vectors[self.p_vec]
-            if self.p_elem < len(curr_vec):
-                ret = curr_vec[self.p_elem]
+        if self.queue:
+            vec_index, elem_index = self.queue.popleft()
+            next_elem_index = elem_index + 1
+            if next_elem_index < len(self.vectors[vec_index]):
+                # append the pointer for the next round
+                # if there are some elements left
+                self.queue.append((vec_index, next_elem_index))
 
-            iter_num += 1
-            self.p_vec = (self.p_vec + 1) % len(self.vectors)
-            # increment the element pointer once iterating all vectors
-            if self.p_vec == 0:
-                self.p_elem += 1
-
-            if ret is not None:
-                self.output_count += 1
-                return ret
+            return self.vectors[vec_index][elem_index]
 
         # no more element to output
         raise Exception
 
-
     def hasNext(self) -> bool:
-        return self.output_count < self.total_num
-
-# Your ZigzagIterator object will be instantiated and called as such:
-# i, v = ZigzagIterator(v1, v2), []
-# while i.hasNext(): v.append(i.next())
+        return len(self.queue) > 0
