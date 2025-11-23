3982af6683e5a30ed5fa9ae003f4a869729015da
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-23 18:44:40 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (19.59%) - LeetSync

--- 250-count-univalue-subtrees/count-univalue-subtrees.cpp
+++ 250-count-univalue-subtrees/count-univalue-subtrees.cpp
@@ -11,34 +11,34 @@
  */
 class Solution {
 public:
-    pair<bool, int> dfs(TreeNode* node) {
-        if (!node) {
-            return {true, 0};
+    bool dfs(TreeNode* node, int& count) {
+        if (node == nullptr) {
+            return true;
         }
 
-        auto left = dfs(node->left);
-        auto right = dfs(node->right);
-        bool isLeftUniValue = left.first;
-        bool isRightUniValue = right.first;
-        int count = left.second + right.second;
+        bool isLeftUniValue = dfs(node->left, count);
+        bool isRightUniValue = dfs(node->right, count);
 
         // If both the children form uni-value subtrees, we compare the value of
-        // chidrens node with the node value.
+        // chidren's node with the node value.
         if (isLeftUniValue && isRightUniValue) {
-            if (node->left && node->val != node->left->val) {
-                return {false, count};
+            if (node->left != nullptr && node->left->val != node->val) {
+                return false;
             }
-            if (node->right && node->val != node->right->val) {
-                return {false, count};
+            if (node->right != nullptr && node->right->val != node->val) {
+                return false;
             }
-            return {true, count + 1};
+            count++;
+            return true;
         }
         // Else if any of the child does not form a uni-value subtree, the subtree
         // rooted at node cannot be a uni-value subtree.
-        return {false, count};
+        return false;
     }
 
     int countUnivalSubtrees(TreeNode* root) {
-        return dfs(root).second;
+        int count = 0;
+        dfs(root, count);
+        return count;
     }
 };
