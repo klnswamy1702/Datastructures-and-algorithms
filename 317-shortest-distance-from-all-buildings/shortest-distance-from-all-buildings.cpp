31ece31c2024ef7863a0f7390611a296596c2716
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-24 01:08:32 +0530

    Time: 143 ms (98.89%) | Memory: 48.7 MB (93.57%) - LeetSync

--- 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.cpp
+++ 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.cpp
@@ -1,85 +1,63 @@
 class Solution {
-private:
-    void bfs(vector<vector<int>>& grid, vector<vector<vector<int>>>& distances, int row, int col) {
-        int dirs[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        
-        int rows = grid.size(), cols = grid[0].size();
-        
-        // Queue to do a bfs, starting from each cell located at (r,c).
-        queue<pair<int, int>> q;
-        q.push({ row, col });
-        
-        // Keep track of visited cells.
-        vector<vector<bool>> vis (rows, vector<bool>(cols, false));
-        vis[row][col] = true;
-        
-        int steps = 0;
-        
-        while (!q.empty()) {
-            for (int i = q.size(); i > 0; --i) {
-                auto curr = q.front();
-                q.pop();
-                row = curr.first;
-                col = curr.second;
-                
-                // If we reached an empty cell, then add the distance
-                // and increment the count of houses reached at this cell.
-                if (grid[row][col] == 0) {
-                    distances[row][col][0] += steps;
-                    distances[row][col][1] += 1;
-                }
-                
-                // Traverse the next cells which is not a blockage.
-                for (auto& dir : dirs) {
-                    int nextRow = row + dir[0];
-                    int nextCol = col + dir[1];
-                    if (nextRow >= 0 && nextCol >= 0 && nextRow < rows && nextCol < cols) {
-                        if (!vis[nextRow][nextCol] && grid[nextRow][nextCol] == 0) {
-                            vis[nextRow][nextCol] = true;
-                            q.push({ nextRow, nextCol });
-                        }
-                    }
-                }
-            }
-            
-            // After traversing one level cells, increment the steps by 1.
-            steps++;
-        }
-    }
-    
 public:
     int shortestDistance(vector<vector<int>>& grid) {
-        int minDistance = INT_MAX;
+        // Next four directions.
+        int dirs[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        
         int rows = grid.size();
         int cols = grid[0].size();
-        int totalHouses = 0;
         
-        // Store { total_dist, houses_count } for each cell.
-        vector<vector<vector<int>>> distances (rows, vector<vector<int>> (cols, {0, 0}));
-        
-        // Count houses and start bfs from each house.
+        // Total Mtrix to store total distance sum for each empty cell.
+        vector<vector<int>> total(rows, vector<int> (cols, 0));
+
+        int emptyLandValue = 0;
+        int minDist = INT_MAX;
+
         for (int row = 0; row < rows; ++row) {
             for (int col = 0; col < cols; ++col) {
+
+                // Start a bfs from each house.
                 if (grid[row][col] == 1) {
-                    totalHouses++;
-                    bfs(grid, distances, row, col);
+                    minDist = INT_MAX;
+
+                    // Use a queue to perform a BFS, starting from the cell located at (row, col).
+                    queue<pair<int, int>> q;
+                    q.push({ row, col });
+                    
+                    int steps = 0;
+
+                    while (!q.empty()) {
+                        steps++;
+
+                        for (int level = q.size(); level > 0; --level) {
+                            auto curr = q.front();
+                            q.pop();
+
+                            for (auto& dir : dirs) {
+                                int nextRow = curr.first + dir[0];
+                                int nextCol = curr.second + dir[1];
+
+                                // For each cell with the value equal to empty land value
+                                // add distance and decrement the cell value by 1.
+                                if (nextRow >= 0 && nextRow < rows &&
+                                    nextCol >= 0 && nextCol < cols &&
+                                    grid[nextRow][nextCol] == emptyLandValue) {
+                                    grid[nextRow][nextCol]--;
+                                    total[nextRow][nextCol] += steps;
+
+                                    q.push({ nextRow, nextCol });
+                                    minDist = min(minDist, total[nextRow][nextCol]);
+                                }
+                            }
+                        }
+                    }
+
+                    // Decrement empty land value to be searched in next iteration.
+                    emptyLandValue--;
                 }
             }
         }
-        
-        // Check all empty lands with houses count equal to total houses and find the min ans.
-        for (int row = 0; row < rows; ++row) {
-            for (int col = 0; col < cols; ++col) {
-                if (distances[row][col][1] == totalHouses) {
-                    minDistance = min(minDistance, distances[row][col][0]);
-                }
-            }
-        }
-        
-        // If we haven't found a valid cell return -1.
-        if (minDistance == INT_MAX) {
-            return -1;
-        }
-        return minDistance;
+
+        return minDist == INT_MAX ? -1 : minDist;
     }
 };
