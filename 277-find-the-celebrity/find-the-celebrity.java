a1f7493203eb47a2db9b3eeece040a87a3992f7f
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-24 00:54:55 +0530

    Time: 82 ms (16.79%) | Memory: 47.2 MB (7.89%) - LeetSync

--- 277-find-the-celebrity/find-the-celebrity.java
+++ 277-find-the-celebrity/find-the-celebrity.java
@@ -1,20 +1,13 @@
-/* The knows API is defined in the parent class Relation.
-      boolean knows(int a, int b); */
-
 public class Solution extends Relation {
     
     private int numberOfPeople;
     
     public int findCelebrity(int n) {
         numberOfPeople = n;
-        int celebrityCandidate = 0;
         for (int i = 0; i < n; i++) {
-            if (knows(celebrityCandidate, i)) {
-                celebrityCandidate = i;
+            if (isCelebrity(i)) {
+                return i;
             }
-        }
-        if (isCelebrity(celebrityCandidate)) {
-            return celebrityCandidate;
         }
         return -1;
     }
