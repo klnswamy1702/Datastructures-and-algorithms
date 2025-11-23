f6c5e79969580e3f474e27014d438d918e810cbb
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-23 18:44:30 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (94.59%) - LeetSync

--- 250-count-univalue-subtrees/count-univalue-subtrees.cpp
+++ 250-count-univalue-subtrees/count-univalue-subtrees.cpp
@@ -11,35 +11,34 @@
  */
 class Solution {
 public:
-    int count = 0;
-
-    bool dfs(TreeNode* node) {
-        if (node == nullptr) {
-            return true;
+    pair<bool, int> dfs(TreeNode* node) {
+        if (!node) {
+            return {true, 0};
         }
 
-        bool isLeftUniValue = dfs(node->left);
-        bool isRightUniValue = dfs(node->right);
+        auto left = dfs(node->left);
+        auto right = dfs(node->right);
+        bool isLeftUniValue = left.first;
+        bool isRightUniValue = right.first;
+        int count = left.second + right.second;
 
         // If both the children form uni-value subtrees, we compare the value of
-        // chidren's node with the node value.
+        // chidrens node with the node value.
         if (isLeftUniValue && isRightUniValue) {
-            if (node->left != nullptr && node->left->val != node->val) {
-                return false;
+            if (node->left && node->val != node->left->val) {
+                return {false, count};
             }
-            if (node->right != nullptr && node->right->val != node->val) {
-                return false;
+            if (node->right && node->val != node->right->val) {
+                return {false, count};
             }
-            count++;
-            return true;
+            return {true, count + 1};
         }
         // Else if any of the child does not form a uni-value subtree, the subtree
         // rooted at node cannot be a uni-value subtree.
-        return false;
+        return {false, count};
     }
 
     int countUnivalSubtrees(TreeNode* root) {
-        dfs(root);
-        return count;
+        return dfs(root).second;
     }
 };
