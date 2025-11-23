6733dfd7d0fd921bbd484cf010384b8b989cee2e
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-24 00:31:04 +0530

    Time: 26 ms (21.11%) | Memory: 39.5 MB (21.11%) - LeetSync

--- 247-strobogrammatic-number-ii/strobogrammatic-number-ii.cpp
+++ 247-strobogrammatic-number-ii/strobogrammatic-number-ii.cpp
@@ -5,33 +5,43 @@ public:
         {'6', '9'}, {'8', '8'}, {'9', '6'}
     };
     
-    vector<string> generateStroboNumbers(int n, int finalLength) {
-        if (n == 0) {
-            // 0-digit strobogrammatic number is an empty string.
-            return { "" };
+    vector<string> findStrobogrammatic(int n) {
+        queue<string> q;
+        int currStringsLength;
+        
+        // When n is even, it means when decreasing by 2 we will go till 0.
+        if (n % 2 == 0) {
+            // We will start with 0-digit strobogrammatic numbers.
+            currStringsLength = 0;
+            q.push("");
+        } else {
+            // We will start with 1-digit strobogrammatic numbers.
+            currStringsLength = 1;
+            q.push("0");
+            q.push("1");
+            q.push("8");
         }
         
-        if (n == 1) {
-            // 1-digit strobogrammatic numbers.
-            return { "0", "1", "8" };
-        }
-        
-        vector<string> prevStroboNums = generateStroboNumbers(n - 2, finalLength);
-        vector<string> currStroboNums;
-        
-        for (string& prevStroboNum : prevStroboNums) {
-            for (vector<char>& pair : reversiblePairs) {
-                // We can only append 0's if it is not first digit.
-                if (pair[0] != '0' || n != finalLength) {
-                    currStroboNums.push_back(pair[0] + prevStroboNum + pair[1]);
+        while (currStringsLength < n) {
+            currStringsLength += 2;
+            for (int i = q.size(); i > 0; --i) {
+                string number = q.front();
+                q.pop();
+                
+                for (vector<char>& pair : reversiblePairs) {
+                    if (currStringsLength != n || pair[0] != '0') {
+                        q.push(pair[0] + number + pair[1]);
+                    }
                 }
             }
         }
         
-        return currStroboNums;
-    }
-    
-    vector<string> findStrobogrammatic(int n) {
-        return generateStroboNumbers(n, n);
+        vector<string> stroboNums;
+        while (!q.empty()) {
+            stroboNums.push_back(q.front());
+            q.pop();
+        }
+        
+        return stroboNums;
     }
 };
