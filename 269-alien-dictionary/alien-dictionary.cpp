7ab77cf50c40644f14bb2124a287dc09f22430cf
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-24 01:05:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (85.11%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.cpp
+++ 269-alien-dictionary/alien-dictionary.cpp
@@ -1,58 +1,58 @@
 class Solution {
+    unordered_map<char, vector<char>> reverseAdjList;
+    unordered_map<char, bool> seen;
+    string output;
+
 public:
     string alienOrder(vector<string>& words) {
-        // Step 0: Create data structures and find all unique letters.
-        unordered_map<char, vector<char>> adjList;
-        unordered_map<char, int> counts;
+        // Step 0: Put all unique letters into reverseAdjList as keys.
         for (string word : words) {
             for (char c : word) {
-                counts[c] = 0;
-                adjList[c] = vector<char>();
+                if (reverseAdjList.find(c) == reverseAdjList.end())
+                    reverseAdjList[c] = vector<char>();
             }
         }
 
-        // Step 1: Find all edges.
+        // Step 1: Find all edges and add reverse edges to reverseAdjList.
         for (int i = 0; i < words.size() - 1; i++) {
             string word1 = words[i];
             string word2 = words[i + 1];
             // Check that word2 is not a prefix of word1.
-            if (word1.size() > word2.size() &&
-                word1.substr(0, word2.size()) == word2) {
+            if (word1.length() > word2.length() &&
+                word1.substr(0, word2.size()) == word2)
                 return "";
-            }
+
             // Find the first non match and insert the corresponding relation.
-            for (int j = 0; j < min(word1.size(), word2.size()); j++) {
+            for (int j = 0; j < min(word1.length(), word2.length()); j++) {
                 if (word1[j] != word2[j]) {
-                    adjList[word1[j]].push_back(word2[j]);
-                    counts[word2[j]]++;
+                    reverseAdjList[word2[j]].push_back(word1[j]);
                     break;
                 }
             }
         }
 
-        // Step 2: Breadth-first search.
-        string sb = "";
-        queue<char> queue;
-        for (auto item : counts) {
-            if (item.second == 0) {
-                queue.push(item.first);
-            }
-        }
-        while (!queue.empty()) {
-            char c = queue.front();
-            queue.pop();
-            sb += c;
-            for (char next : adjList[c]) {
-                counts[next]--;
-                if (counts[next] == 0) {
-                    queue.push(next);
-                }
-            }
+        // Step 2: DFS to build up the output list.
+        for (auto const& iter : reverseAdjList) {
+            bool result = dfs(iter.first);
+            if (!result) return "";
         }
 
-        if (sb.size() < counts.size()) {
-            return "";
+        return output;
+    }
+
+    // Return true iff no cycles detected.
+    bool dfs(char c) {
+        if (seen.find(c) != seen.end()) {
+            // If this node was grey (false), a cycle was detected.
+            return seen[c];
         }
-        return sb;
+        seen[c] = false;
+        for (char next : reverseAdjList[c]) {
+            bool result = dfs(next);
+            if (!result) return false;
+        }
+        seen[c] = true;
+        output += c;
+        return true;
     }
 };
