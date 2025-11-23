58dd334d3bd1d37cff7321ec44e320271c324497
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-24 01:14:27 +0530

    Time: 1726 ms (5.09%) | Memory: 208.9 MB (5.09%) - LeetSync

--- 1142-minimum-knight-moves/minimum-knight-moves.java
+++ 1142-minimum-knight-moves/minimum-knight-moves.java
@@ -4,37 +4,50 @@ class Solution {
         int[][] offsets = {{1, 2}, {2, 1}, {2, -1}, {1, -2},
                 {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}};
 
-        // - Rather than using the inefficient HashSet, we use the bitmap
-        //     otherwise we would run out of time for the test cases.
-        // - We create a bitmap that is sufficient to cover all the possible
-        //     inputs, according to the description of the problem.
-        boolean[][] visited = new boolean[607][607];
+        // data structures needed to move from the origin point
+        Deque<int[]> originQueue = new LinkedList<>();
+        originQueue.addLast(new int[]{0, 0, 0});
+        Map<String, Integer> originDistance = new HashMap<>();
+        originDistance.put("0,0", 0);
 
-        Deque<int[]> queue = new LinkedList<>();
-        queue.addLast(new int[]{0, 0});
-        int steps = 0;
+        // data structures needed to move from the target point
+        Deque<int[]> targetQueue = new LinkedList<>();
+        targetQueue.addLast(new int[]{x, y, 0});
+        Map<String, Integer> targetDistance = new HashMap<>();
+        targetDistance.put(x + "," + y, 0);
 
-        while (queue.size() > 0) {
-            int currLevelSize = queue.size();
-            // iterate through the current level
-            for (int i = 0; i < currLevelSize; i++) {
-                int[] curr = queue.removeFirst();
-                if (curr[0] == x && curr[1] == y) {
-                    return steps;
+        while (true) {
+            // check if we reach the circle of target
+            int[] origin = originQueue.removeFirst();
+            String originXY = origin[0] + "," + origin[1];
+            if (targetDistance.containsKey(originXY)) {
+                return origin[2] + targetDistance.get(originXY);
+            }
+
+            // check if we reach the circle of origin
+            int[] target = targetQueue.removeFirst();
+            String targetXY = target[0] + "," + target[1];
+            if (originDistance.containsKey(targetXY)) {
+                return target[2] + originDistance.get(targetXY);
+            }
+
+            for (int[] offset : offsets) {
+                // expand the circle of origin
+                int[] nextOrigin = new int[]{origin[0] + offset[0], origin[1] + offset[1]};
+                String nextOriginXY = nextOrigin[0] + "," + nextOrigin[1];
+                if (!originDistance.containsKey(nextOriginXY)) {
+                    originQueue.addLast(new int[]{nextOrigin[0], nextOrigin[1], origin[2] + 1});
+                    originDistance.put(nextOriginXY, origin[2] + 1);
                 }
 
-                for (int[] offset : offsets) {
-                    int[] next = new int[]{curr[0] + offset[0], curr[1] + offset[1]};
-                    // align the coordinate to the bitmap
-                    if (!visited[next[0] + 302][next[1] + 302]) {
-                        visited[next[0] + 302][next[1] + 302] = true;
-                        queue.addLast(next);
-                    }
+                // expand the circle of target
+                int[] nextTarget = new int[]{target[0] + offset[0], target[1] + offset[1]};
+                String nextTargetXY = nextTarget[0] + "," + nextTarget[1];
+                if (!targetDistance.containsKey(nextTargetXY)) {
+                    targetQueue.addLast(new int[]{nextTarget[0], nextTarget[1], target[2] + 1});
+                    targetDistance.put(nextTargetXY, target[2] + 1);
                 }
             }
-            steps++;
         }
-        // move on to the next level
-        return steps;
     }
 }
