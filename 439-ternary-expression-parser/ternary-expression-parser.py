f3f004b2d235ade71b06787f3fcd5682af996c02
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-23 18:34:19 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (55.21%) - LeetSync

--- 439-ternary-expression-parser/ternary-expression-parser.py
+++ 439-ternary-expression-parser/ternary-expression-parser.py
@@ -1,40 +1,20 @@
-class TreeNode:
-    def __init__(self, val):
-        self.val = val
-        self.left = None
-        self.right = None
-
 class Solution:
     def parseTernary(self, expression: str) -> str:
-        
-        # Global Index to Construct Binary Tree
-        self.index = 0
-        root = self.constructTree(expression)
-        
-        # Parse the binary tree till we reach the leaf node
-        while root.left and root.right:
-            if root.val == 'T':
-                root = root.left
+
+        i = 0
+        while True:
+
+            if expression[i] not in 'TF' or i == len(expression) - 1\
+            or expression[i + 1] == ':':
+                return expression[i]
+            if expression[i] == 'T':
+                i = i + 2
             else:
-                root = root.right
-        
-        return root.val
-
-    def constructTree(self, expression):
-        
-        # Storing current character of expression
-        root = TreeNode(expression[self.index])
-
-        # If the last character of expression, return
-        if self.index == len(expression) - 1:
-            return root
-        
-        # Check the next character
-        self.index += 1
-        if expression[self.index] == '?':
-            self.index += 1
-            root.left = self.constructTree(expression)
-            self.index += 1
-            root.right = self.constructTree(expression)
-            
-        return root
+                count = 1
+                i = i + 2
+                while count != 0:
+                    if expression[i] == ':':
+                        count -= 1
+                    elif expression[i] == '?':
+                        count += 1
+                    i += 1
