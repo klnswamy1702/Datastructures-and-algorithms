69d5703b1a44c46a44f119e292c4bb5efd35f238
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-23 18:33:31 +0530

    Time: 7 ms (75.46%) | Memory: 17.7 MB (79.75%) - LeetSync

--- 439-ternary-expression-parser/ternary-expression-parser.py
+++ 439-ternary-expression-parser/ternary-expression-parser.py
@@ -1,21 +1,24 @@
 class Solution:
     def parseTernary(self, expression: str) -> str:
-
-        # Reduce expression until we are left with a single character
-        while len(expression) != 1:
-            questionMarkIndex = len(expression) - 1
-            while expression[questionMarkIndex] != '?':
-                questionMarkIndex -= 1
-
-            # Find the value of the expression.
-            if expression[questionMarkIndex - 1] == 'T':
-                value = expression[questionMarkIndex + 1]
+        
+        # Initialize a stack
+        stack = []
+        
+        # Traverse the expression from right to left
+        for char in expression[::-1]:
+            
+            # If stack top is ?, then replace next four characters
+            # with E1 or E2 depending on the value of B
+            if stack and stack[-1] == '?':
+                stack.pop()
+                onTrue = stack.pop()
+                stack.pop()
+                onFalse = stack.pop()
+                stack.append(onTrue if char == 'T' else onFalse)
+            
+            # Otherwise, push this character
             else:
-                value = expression[questionMarkIndex + 3]
-
-            # Replace the expression with the value
-            expression = expression[:questionMarkIndex - 1] + value\
-                + expression[questionMarkIndex + 4:]
-
+                stack.append(char)
+        
         # Return the final character
-        return expression
+        return stack[0]
