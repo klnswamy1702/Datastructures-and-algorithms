8068a002c1e2c3f359b62a1fffa621a49b239c12
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2025-11-24 00:59:32 +0530

    Time: 51 ms (95.45%) | Memory: 53.5 MB (42.42%) - LeetSync

--- 1052-campus-bikes/campus-bikes.cpp
+++ 1052-campus-bikes/campus-bikes.cpp
@@ -6,21 +6,20 @@ public:
     }
     
     vector<int> assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes) {
-        // List of WorkerBikeTuples's to store all the possible triplets
-        vector<tuple<int, int, int>> allTriplets;
+        int minDis = INT_MAX;
+        // Stores the list of (worker, bike) pairs corresponding to its distance
+        vector<pair<int, int>> disToPairs[1999];
         
-        // Generate all the possible pairs
+        // Add the (worker, bike) pairs corresponding to their distance list
         for (int worker = 0; worker < workers.size(); worker++) {
             for (int bike = 0; bike < bikes.size(); bike++) {
-                int distance = findDistance(workers[worker], bikes[bike]);        
-                allTriplets.push_back({distance, worker, bike});
+                int distance = findDistance(workers[worker], bikes[bike]);
+                disToPairs[distance].push_back({worker, bike});
+                minDis = min(minDis, distance);
             }
         }
         
-        // Sort the triplets. By default, each sorting will prioritize the
-        // Tuple's first value, then second value, and finally the third value
-        sort(allTriplets.begin(), allTriplets.end());  
-        
+        int currDis = minDis;
         // Initialize all values to false, to signify no bikes have been taken
         vector<int> bikeStatus(bikes.size(), false);
         // Initialize all index to -1, to signify no worker has a bike
@@ -28,18 +27,17 @@ public:
         // Keep track of how many worker-bike pairs have been made
         int pairCount = 0;
         
-        for (auto[dist, worker, bike] : allTriplets) { 
-            // If both worker and bike are free, assign them to each other
-            if (workerStatus[worker] == -1 && !bikeStatus[bike]) {
-                bikeStatus[bike] = true;
-                workerStatus[worker] = bike;
-                pairCount++;
-                
-                // If all the workers have the bike assigned, we can stop
-                if (pairCount == workers.size()) {
-                    return workerStatus;
+        // Until all workers have not been assigned a bike
+        while (pairCount != workers.size()) {
+            for (auto[worker, bike] : disToPairs[currDis]) {
+                if (workerStatus[worker] == -1 && !bikeStatus[bike]) {
+                    // If both worker and bike are free, assign them to each other
+                    bikeStatus[bike] = true;
+                    workerStatus[worker] = bike;
+                    pairCount++;
                 }
             }
+            currDis++;
         }
         
         return workerStatus;
