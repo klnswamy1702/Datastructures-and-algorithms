f613636d85f8395a9b0da41a6e7abddac0c10105
Karampuri Laxmi Narsimha Swamy <81169765+klnswamy1702@users.noreply.github.com>
2026-05-22 15:37:39 +0530

    Time: 19 ms (70.44%) | Memory: 54.5 MB (38.74%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,33 +1,21 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int n = s.length();
-        int m = t.length();
-        /* Declare an array to store the count of 
-        distinct subsequences for each character in t*/
-        int[] prev = new int[m + 1];
+        int m = s.length(), n = t.length();
+        int[][] dp = new int[m + 1][n + 1];
 
-        // Initialize the count for an empty string 
-        prev[0] = 1;
+        for (int i = 0; i <= m; i++) dp[i][0] = 1;
 
-        // Iterate through s and t to calculate counts
-        for (int i = 1; i <= n; i++) {
-            /* Iterate in reverse direction to
-            avoid overwriting values prematurely*/
-            for (int j = m; j >= 1; j--) {
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+
                 if (s.charAt(i - 1) == t.charAt(j - 1)) {
-                    /* If the characters match, consider two options:
-                    either leave one character in s and t or leave 
-                    one character in s and continue matching t*/
-                    prev[j] = (prev[j - 1] + prev[j]);
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
                 }
-                /* No need for an else statement since we
-                can simply leave the previous count as is*/
             }
         }
 
-        /* The value at prev[m] contains
-        the count of distinct subsequences*/
-        return prev[m];
-        
+        return dp[m][n];
     }
 }
